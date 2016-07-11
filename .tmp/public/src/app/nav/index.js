bd991e1f41ce0efa6beb8af46bddfc1b68046bec
Trevor Overman <troverman@gmail.com>
2016-07-11 14:00:19 -0400

    ohhhh

--- .tmp/public/src/app/nav/index.js
+++ .tmp/public/src/app/nav/index.js
@@ -3,8 +3,7 @@ angular.module( 'voetr.nav', [
 
 .controller( 'NavCtrl', function NavController( $scope, $state, config, $rootScope ) {
     $scope.currentUser = config.currentUser;
-    $rootScope.$on("$stateChangeSuccess", function() {
-    	console.log('ok')
+   	$rootScope.$on("$stateChangeSuccess", function() {
         window.scrollTo(0, 0);
     });
 });
--- assets/src/app/nav/index.js
+++ assets/src/app/nav/index.js
@@ -3,7 +3,7 @@ angular.module( 'voetr.nav', [
 
 .controller( 'NavCtrl', function NavController( $scope, $state, config, $rootScope ) {
     $scope.currentUser = config.currentUser;
-    $rootScope.$on("$stateChangeSuccess", function() {
+   	$rootScope.$on("$stateChangeSuccess", function() {
         window.scrollTo(0, 0);
     });
 });
