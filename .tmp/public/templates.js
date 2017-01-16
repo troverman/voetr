10df972ed1a171a2765a0fb5781b4e60d26114a3
evolvedus <evolvedus@gmail.com>
2017-01-15 20:57:48 -0500

    nav, intro header, login/register

--- .tmp/public/templates.js
+++ .tmp/public/templates.js
@@ -3,10 +3,15 @@ angular.module('templates-app', ['about/index.tpl.html', 'account/index.tpl.html
 angular.module("about/index.tpl.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("about/index.tpl.html",
     "<div class=\"imageContainer\">\n" +
-    "	<video class='flexible' autoplay=\"autoplay\" muted=\"muted\" preload=\"auto\" loop=\"loop\"><source src=\"https://s3-us-west-2.amazonaws.com/voetr/washington.mp4\" type=\"video/mp4\"></video>\n" +
-    "	<div class=\"imageContainerDiv\">  \n" +
-    "		<h1>build empowerment, change consensus</h1>\n" +
-    "	</div>\n" +
+    "    <video class=\"flexible\" autoplay=\"autoplay\" muted=\"muted\" preload=\"auto\" loop=\"loop\">\n" +
+    "        <source src=\"https://s3-us-west-2.amazonaws.com/voetr/washington.mp4\" type=\"video/mp4\">\n" +
+    "    </video>\n" +
+    "    <div class=\"imageContainerDiv\">\n" +
+    "        <div class=\"intro-text\">\n" +
+    "            <h1 class=\"light vBlue\">foster empowerment, change consensus</h1>\n" +
+    "            <h1><b>are they listening?</b></h1>\n" +
+    "        </div>\n" +
+    "    </div>\n" +
     "</div>\n" +
     "<!--\n" +
     "<div class=\"heading-container\">\n" +
@@ -804,96 +809,50 @@ angular.module("home/index.tpl.html", []).run(["$templateCache", function($templ
 
 angular.module("intro/index.tpl.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("intro/index.tpl.html",
-    "<div class=\"intro-container intro\" style=\"max-height:700px\">\n" +
-    "    <svg class=\"intro\" style=\"max-height:700px\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 1920 1080\" width=\"100%\" height=\"100%\" preserveAspectRatio=\"xMidYMid slice\">\n" +
-    "        <defs>\n" +
-    "            <mask class=\"intro-mask\" id=\"intro-mask\" x=\"0\" y=\"0\" width=\"100%\" height=\"100%\">\n" +
-    "                <rect class=\"intro-rect\" x=\"0\" y=\"0\" width=\"1920px\" height=\"1080px\"></rect>\n" +
-    "                <text x=\"960\" y=\"40%\" class=\"medium-text desktop\">build empowerment, change consensus</text>\n" +
-    "                <text x=\"960\" y=\"38%\" class=\"medium-text mobile\">build empowerment, change consensus</text>\n" +
-    "                <text x=\"960\" y=\"45%\" class=\"small-text mantra\">with direct input on policy, direct your impact</text>\n" +
-    "                <text x=\"960\" y=\"67.5%\" class=\"small-text learn-more\">learn more</text>\n" +
-    "                <a href=\"#about\" du-smooth-scroll>\n" +
-    "                    <svg class=\"tri-before\" version=\"1.1\" id=\"Layer_1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" x=\"883px\" y=\"68%\" width=\"150px\" height=\"150px\" viewBox=\"0 0 723 626\" enable-background=\"new 0 0 723 626\" xml:space=\"preserve\">\n" +
-    "                        <g>\n" +
-    "                          <path fill=\"#232322\" d=\"M0,0h723v1.58c-3.72,4.37-5.58,9.96-8.89,14.62C596.69,219.439,479.44,422.79,361.96,626h-0.6\n" +
-    "                            C243.45,421.77,125.54,217.55,7.63,13.32C5.09,9.061,2.85,4.62,0,0.55V0z\"></path>\n" +
-    "                          <path fill=\"#FFFFFF\" d=\"M86.57,48.002c183.289,0.01,366.569-0.02,549.859,0.02c-40.779,70.681-81.6,141.33-122.39,212.01\n" +
-    "                            c-7.36,12.841-14.91,25.58-22.11,38.521l-0.229-0.01c-86.79-0.09-173.59-0.06-260.38-0.011h-0.211\n" +
-    "                            c-20.239-35.569-40.899-70.899-61.289-106.379C142.061,144.111,114.359,96.031,86.57,48.002z\"></path>\n" +
-    "                          <path fill=\"#21B795\" d=\"M231.311,298.599c86.79-0.049,173.59-0.079,260.38,0.011c-19.42,34.3-39.36,68.31-59,102.479\n" +
-    "                            c-23.71,41.091-47.44,82.16-71.15,123.25c-20.92-36.039-41.68-72.17-62.55-108.229C276.48,376.909,253.65,337.89,231.311,298.599z\"></path>\n" +
-    "                        </g>\n" +
-    "                    </svg>\n" +
-    "                </a>\n" +
-    "            </mask>\n" +
-    "        </defs>\n" +
-    "        <rect class=\"intro-rect\" x=\"0\" y=\"0\" width=\"100%\" height=\"100%\"></rect>\n" +
-    "        <a href=\"#about\" class=\"hvr-bob\" du-smooth-scroll>\n" +
-    "            <svg class=\"tri-after\" version=\"1.1\" id=\"Layer_1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" x=\"883px\" y=\"68%\" width=\"150px\" height=\"150px\" viewBox=\"0 0 723 626\" enable-background=\"new 0 0 723 626\" xml:space=\"preserve\">\n" +
-    "                <g>\n" +
-    "                  <path fill=\"#232322\" d=\"M0,0h723v1.58c-3.72,4.37-5.58,9.96-8.89,14.62C596.69,219.439,479.44,422.79,361.96,626h-0.6\n" +
-    "                    C243.45,421.77,125.54,217.55,7.63,13.32C5.09,9.061,2.85,4.62,0,0.55V0z\"></path>\n" +
-    "                  <path fill=\"#FFFFFF\" d=\"M86.57,48.002c183.289,0.01,366.569-0.02,549.859,0.02c-40.779,70.681-81.6,141.33-122.39,212.01\n" +
-    "                    c-7.36,12.841-14.91,25.58-22.11,38.521l-0.229-0.01c-86.79-0.09-173.59-0.06-260.38-0.011h-0.211\n" +
-    "                    c-20.239-35.569-40.899-70.899-61.289-106.379C142.061,144.111,114.359,96.031,86.57,48.002z\"></path>\n" +
-    "                  <path fill=\"#21B795\" d=\"M231.311,298.599c86.79-0.049,173.59-0.079,260.38,0.011c-19.42,34.3-39.36,68.31-59,102.479\n" +
-    "                    c-23.71,41.091-47.44,82.16-71.15,123.25c-20.92-36.039-41.68-72.17-62.55-108.229C276.48,376.909,253.65,337.89,231.311,298.599z\"></path>\n" +
-    "                </g>\n" +
-    "            </svg>\n" +
-    "        </a>\n" +
-    "    </svg> \n" +
-    "\n" +
-    "    <video id=\"video\" autoplay=\"autoplay\" muted=\"muted\" preload=\"auto\" loop=\"loop\">\n" +
-    "        <source src=\"https://s3-us-west-2.amazonaws.com/voetr/washington.mp4\" type=\"video/webm\">\n" +
+    "<div class=\"imageContainer\">\n" +
+    "    <video class=\"flexible\" autoplay=\"autoplay\" muted=\"muted\" preload=\"auto\" loop=\"loop\">\n" +
+    "        <source src=\"https://s3-us-west-2.amazonaws.com/voetr/washington.mp4\" type=\"video/mp4\">\n" +
     "    </video>\n" +
-    "</div>\n" +
-    "");
+    "    <div class=\"imageContainerDiv\">\n" +
+    "        <div class=\"intro-text\">\n" +
+    "            <h1 class=\"light vBlue\">foster empowerment, change consensus</h1>\n" +
+    "            <h1><b>are they listening?</b></h1>\n" +
+    "            <div class=\"btn-cont\">\n" +
+    "                <a class=\"btn\" href=\"#about\" du-smooth-scroll>\n" +
+    "                    Talk Louder\n" +
+    "                    <span class=\"line-1\"></span>\n" +
+    "                    <span class=\"line-2\"></span>\n" +
+    "                    <span class=\"line-3\"></span>\n" +
+    "                    <span class=\"line-4\"></span>\n" +
+    "                </a>\n" +
+    "            </div>\n" +
+    "        </div>\n" +
+    "    </div>\n" +
+    "</div>");
 }]);
 
 angular.module("login/index.tpl.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("login/index.tpl.html",
-    "<!--login-->\n" +
-    "<div class=\"login-form container\" style=\"min-height:100%;\">\n" +
-    "    <div class=\"row\">\n" +
-    "        <div class=\"col-md-12\">\n" +
-    "            <h3>Login</h3><hr>\n" +
-    "            <form class=\"form-horizontal\" role=\"form\" action=\"/auth/local\" method=\"post\">\n" +
-    "                <div class=\"form-group\">\n" +
-    "                    <label for=\"inputUsername3\" class=\"col-sm-2 control-label\">Username</label>\n" +
-    "                    <div class=\"col-sm-10\">\n" +
-    "                        <input type=\"text\" class=\"form-control\" id=\"inputUsername3\" name=\"identifier\" placeholder=\"Username\">\n" +
-    "                    </div>\n" +
-    "                </div>\n" +
-    "                <div class=\"form-group\">\n" +
-    "                    <label for=\"inputPassword3\" class=\"col-sm-2 control-label\">Password</label>\n" +
-    "                    <div class=\"col-sm-10\">\n" +
-    "                        <input type=\"password\" class=\"form-control\" id=\"inputPassword3\" name=\"password\" placeholder=\"Password\">\n" +
-    "                    </div>\n" +
-    "                </div>\n" +
-    "                <div class=\"form-group\">\n" +
-    "                    <div class=\"col-sm-offset-1 col-sm-10\">\n" +
-    "                        <button type=\"submit\" class=\"btn btn-default\">Sign in</button>\n" +
-    "                    </div>\n" +
-    "                </div>\n" +
-    "            </form>\n" +
-    "        </div>\n" +
-    "    </div>\n" +
-    "    <hr>\n" +
-    "    <!--<div class=\"row\">\n" +
-    "        <div style=\"text-align:center\">\n" +
-    "            <a href=\"/auth/facebook\">facebook</a>\n" +
-    "            <a href=\"/auth/twitter\">twitter</a>\n" +
-    "            <a href=\"/auth/google\">google</a>\n" +
-    "        </div>\n" +
-    "    </div>-->\n" +
-    "    <div class=\"row\">\n" +
-    "        <div style=\"text-align:center\">\n" +
-    "            <a href=\"/register\">register an account</a>\n" +
-    "        </div>\n" +
+    "<div class=\"log-background\">\n" +
+    "    <div class=\"blkoverlay\"></div>\n" +
+    "    <div class=\"log-form\">\n" +
+    "        <h2><span class=\"inline-logo\"><i style=\"color:#2ab996\" class=\"fa fa-check\"></i>oetr</span> - login</h2>\n" +
+    "        <form role=\"form\" action=\"/auth/local\" method=\"post\">\n" +
+    "            <input placeholder=\"username\" title=\"username\" type=\"text\"> \n" +
+    "            <input placeholder=\"password\" title=\"username\" type=\"password\"> \n" +
+    "            <button class=\"btn\" type=\"submit\">sign in</button> \n" +
+    "            <a class=\"already\" href=\"/register\">register an account</a>\n" +
+    "        </form>\n" +
     "    </div>\n" +
     "</div>\n" +
-    "<!--<div ng-include=\"'footer/index.tpl.html'\"></div>-->\n" +
+    "\n" +
+    "<!--<div class=\"row\">\n" +
+    "    <div style=\"text-align:center\">\n" +
+    "        <a href=\"/auth/facebook\">facebook</a>\n" +
+    "        <a href=\"/auth/twitter\">twitter</a>\n" +
+    "        <a href=\"/auth/google\">google</a>\n" +
+    "    </div>\n" +
+    "</div>-->\n" +
     "");
 }]);
 
@@ -1023,13 +982,12 @@ angular.module("member/index.tpl.html", []).run(["$templateCache", function($tem
 angular.module("nav/index.tpl.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("nav/index.tpl.html",
     "<style>\n" +
-    ".navbar-inverse{background-color:rgba(36,36,46,1);}\n" +
+    ".navbar-inverse{background-color:rgb(26,26,26);}\n" +
     "</style>\n" +
-    "\n" +
-    "<div ng-controller=\"NavCtrl\" class=\"navbar navbar-inverse navbar-fixed-top\" role=\"navigation\">\n" +
-    "  <div class=\"container center\">\n" +
+    "<nav ng-controller=\"NavCtrl\" class=\"navbar navbar-inverse navbar-fixed-top\">\n" +
+    "  <div class=\"container\">\n" +
     "    <div class=\"navbar-header\">\n" +
-    "      <button type=\"button\" class=\"navbar-toggle\" data-toggle=\"collapse\" data-target=\".navbar-collapse\">\n" +
+    "      <button type=\"button\" class=\"navbar-toggle collapsed\" data-toggle=\"collapse\" data-target=\".navbar-collapse\" aria-expanded=\"false\">\n" +
     "        <span class=\"sr-only\">Toggle navigation</span>\n" +
     "        <span class=\"icon-bar\"></span>\n" +
     "        <span class=\"icon-bar\"></span>\n" +
@@ -1038,82 +996,76 @@ angular.module("nav/index.tpl.html", []).run(["$templateCache", function($templa
     "      <a class=\"navbar-brand\" href=\"/\"><i style=\"color:#2ab996\" class=\"fa fa-check\"></i>oetr</a>\n" +
     "    </div>\n" +
     "    <div class=\"collapse navbar-collapse\">\n" +
-    "      <ul class=\"nav navbar-nav\">\n" +
+    "      <ul class=\"nav navbar-nav navbar-right\">\n" +
+    "        <form class=\"navbar-form pull-left\" role=\"search\" action=\"/search/\" onSubmit=\" location.href = 'search/' + document.getElementById('search-link').value; return false;\">\n" +
+    "          <div class=\"form-group\">\n" +
+    "            <input ng-keyup=\"keyPress(searchValue)\" ng-model=\"searchValue\" id=\"search-link\" size=\"40\" type=\"text\" placeholder=\" Search (Locations, Reps, etc.) \">\n" +
+    "          </div>\n" +
+    "        </form>\n" +
     "        <li ng-show=\"!currentUser\"><a href=\"/about\">about</a></li>\n" +
     "        <li><a href=\"/search\">discover</a></li>\n" +
     "\n" +
-    "        <form class=\"navbar-form pull-left\" role=\"search\" action=\"/search/\" onSubmit=\" location.href = 'search/' + document.getElementById('search-link').value; return false;\">\n" +
-    "          <div class=\"form-group\">\n" +
-    "            <input ng-keyup=\"keyPress(searchValue)\" ng-model=\"searchValue\" id=\"search-link\" size=\"40\" type=\"text\" placeholder=\"\">\n" +
-    "          </div>\n" +
-    "        </form>\n" +
     "\n" +
-    "        <!--<li ng-show=\"currentUser\"><a href=\"/account\">account</a></li>-->\n" +
-    "        <li ng-show=\"currentUser\"><a href=\"/account\">{{currentUser.username}}</a></li>\n" +
+    "        <li class=\"dropdown\" ng-show=\"!currentUser\">\n" +
+    "          <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\" role=\"button\" aria-haspopup=\"true\" aria-expanded=\"false\">\n" +
+    "            <i class=\"fa fa-user\"></i> <span class=\"caret\"></span>\n" +
+    "          </a>\n" +
+    "          <ul class=\"dropdown-menu\">\n" +
+    "            <li><a href=\"/register\">register</a></li>\n" +
+    "            <li role=\"separator\" class=\"divider\"></li>\n" +
+    "            <li><a href=\"/login\">login</a></li>\n" +
+    "          </ul>\n" +
+    "        </li>\n" +
     "\n" +
-    "        <li ng-show=\"currentUser\"><a href=\"/logout\">signout</a></li>\n" +
-    "        <li ng-show=\"!currentUser\"><a href=\"/register\">register</a></li>\n" +
-    "        <li ng-show=\"!currentUser\"><a href=\"/login\">login</a></li>\n" +
+    "        <li class=\"dropdown\" ng-show=\"currentUser\">\n" +
+    "          <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\" role=\"button\" aria-haspopup=\"true\" aria-expanded=\"false\">\n" +
+    "            {{currentUser.username}} <span class=\"caret\"></span>\n" +
+    "          </a>\n" +
+    "          <ul class=\"dropdown-menu\">\n" +
+    "            <li><a href=\"/account\">account</a></li>\n" +
+    "            <li role=\"separator\" class=\"divider\"></li>\n" +
+    "            <li><a href=\"/logout\">signout</a></li>\n" +
+    "          </ul>\n" +
+    "        </li>\n" +
     "      </ul>\n" +
-    "    </div><!--/.nav-collapse -->\n" +
+    "    </div>\n" +
     "  </div>\n" +
-    "</div>");
+    "</nav>");
 }]);
 
 angular.module("register/index.tpl.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("register/index.tpl.html",
-    "<!--register-->\n" +
-    "<div class=\"register-form container\">\n" +
-    "    <div class=\"row\">\n" +
-    "        <div class=\"col-md-12\">\n" +
-    "            <h3>Create an Account</h3><hr>\n" +
-    "            <form class=\"form-horizontal\" role=\"form\" action=\"/auth/local/register\" method=\"post\">\n" +
-    "                <div class=\"form-group\">\n" +
-    "                    <label for=\"inputUsername3\" class=\"col-sm-2 control-label\">Username</label>\n" +
-    "                    <div class=\"col-sm-10\">\n" +
-    "                        <input type=\"text\" class=\"form-control\" id=\"inputUsername3\" name=\"username\" placeholder=\"Username\" value=\"\">\n" +
-    "                    </div>\n" +
-    "                </div>\n" +
-    "                <div class=\"form-group\">\n" +
-    "                    <label for=\"inputEmail3\" class=\"col-sm-2 control-label\">Email</label>\n" +
-    "                    <div class=\"col-sm-10\">\n" +
-    "                        <input type=\"email\" class=\"form-control\" id=\"inputEmail3\" name=\"email\" placeholder=\"Email\" value=\"\">\n" +
-    "                    </div>\n" +
-    "                </div>\n" +
-    "                <!--<div class=\"form-group\">\n" +
-    "                    <label for=\"inputFirstName3\" class=\"col-sm-2 control-label\">First Name</label>\n" +
-    "                    <div class=\"col-sm-10\">\n" +
-    "                        <input type=\"text\" class=\"form-control\" id=\"inputFirstName3\" name=\"firstName\" placeholder=\"First Name\" value=\"\">\n" +
-    "                    </div>\n" +
-    "                </div>-->\n" +
-    "                <div class=\"form-group\">\n" +
-    "                    <label for=\"inputPassword3\" class=\"col-sm-2 control-label\">Password</label>\n" +
-    "                    <div class=\"col-sm-10\">\n" +
-    "                        <input type=\"password\" class=\"form-control\" id=\"inputPassword3\" name=\"password\" placeholder=\"Password\" value=\"\">\n" +
-    "                    </div>\n" +
-    "                </div>\n" +
-    "                <div class=\"form-group\" style=\"\">\n" +
-    "                    <div class=\"col-sm-offset-1 col-sm-10\">\n" +
-    "                        <button type=\"submit\" class=\"btn btn-default\">Sign in</button>\n" +
-    "                    </div>\n" +
-    "                </div>\n" +
+    "<div class=\"row\">\n" +
+    "    <div class=\"log-background\">\n" +
+    "        <div class=\"blkoverlay\"></div>\n" +
+    "        <div class=\"log-form\">\n" +
+    "            <h2><span class=\"inline-logo\"><i style=\"color:#2ab996\" class=\"fa fa-check\"></i>oetr</span> - register</h2>\n" +
+    "            <form role=\"form\" action=\"/auth/local/register\" method=\"post\">\n" +
+    "                <input placeholder=\"username\" title=\"username\" type=\"text\"> \n" +
+    "                <input placeholder=\"password\" title=\"username\" type=\"password\"> \n" +
+    "                <button class=\"btn\" type=\"submit\">register</button> \n" +
+    "                <a class=\"already\" href=\"/login\">have an account?</a>\n" +
     "            </form>\n" +
     "        </div>\n" +
     "    </div>\n" +
-    "    <hr>\n" +
-    "    <!--<div class=\"row\">\n" +
+    "</div>\n" +
+    "<!--\n" +
+    "<div class=\"row\">\n" +
     "        <div style=\"text-align:center\">\n" +
     "            <a href=\"/auth/facebook\">facebook</a>\n" +
     "            <a href=\"/auth/twitter\">twitter</a>\n" +
     "            <a href=\"/auth/google\">google</a>\n" +
     "        </div>\n" +
-    "    </div>-->\n" +
-    "    <div class=\"row\">\n" +
-    "        <div style=\"text-align:center\">\n" +
-    "            <a href=\"/login\">already have an account?</a>\n" +
+    "    </div>\n" +
+    "    <div class=\"form-group\">\n" +
+    "        <label for=\"inputFirstName3\" class=\"col-sm-2 control-label\">First Name</label>\n" +
+    "        <div class=\"col-sm-10\">\n" +
+    "            <input type=\"text\" class=\"form-control\" id=\"inputFirstName3\" name=\"firstName\" placeholder=\"First Name\" value=\"\">\n" +
     "        </div>\n" +
     "    </div>\n" +
-    "</div>");
+    "-->\n" +
+    "\n" +
+    "");
 }]);
 
 angular.module("search/index.tpl.html", []).run(["$templateCache", function($templateCache) {
