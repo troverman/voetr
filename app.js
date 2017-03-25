81eabed503d176bd8d7d480c743311f92a831064
Trevor Overman <troverman@gmail.com>
2017-03-25 16:25:34 -0400

    test

--- app.js
+++ app.js
@@ -4,7 +4,13 @@
  */
 // Ensure we're in the project directory, so relative paths work as expected
 // no matter where we actually lift from.
-process.chdir(__dirname);
+  var express = require('express');
+  var app = express();
+  app.get('/.well-known/acme-challenge/26yeRHjK-mgWP_Uz4bZP6OsQ-fslBu14HN9WCkfu5zo', function(req, res) {
+    res.send('26yeRHjK-mgWP_Uz4bZP6OsQ-fslBu14HN9WCkfu5zo.yMz-EAV5agQah1zn-w6Aqp0JVzxv1jmSFH6dh5Ea9uI')
+  });
+
+/*process.chdir(__dirname);
 
 // Ensure a "sails" can be located:
 (function() {
@@ -15,11 +21,6 @@ process.chdir(__dirname);
     console.log(e)
     return;
   }
-  var express = require('express');
-  var app = express();
-  app.get('/.well-known/acme-challenge/26yeRHjK-mgWP_Uz4bZP6OsQ-fslBu14HN9WCkfu5zo', function(req, res) {
-    res.send('26yeRHjK-mgWP_Uz4bZP6OsQ-fslBu14HN9WCkfu5zo.yMz-EAV5agQah1zn-w6Aqp0JVzxv1jmSFH6dh5Ea9uI')
-  });
 
   // Try to get `rc` dependency
   var rc;
@@ -35,4 +36,4 @@ process.chdir(__dirname);
 
   // Start server
   sails.lift(rc('sails'));
-})();
+})();*/
