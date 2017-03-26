3830989f60db038d3b37e19aa48eecb79998c620
Trevor Overman <troverman@gmail.com>
2017-03-25 22:01:05 -0400

    ok

--- api/policies/enforceSsl.js
+++ api/policies/enforceSsl.js
@@ -3,7 +3,7 @@ module.exports = function(req, res, next) {
 	if ((req.headers['x-forwarded-proto'] !== 'https') && (process.env.NODE_ENV === 'production')) {
 		return res.redirect([
 			'https://',
-			req.get('Host'),
+			req.host,
 			req.url
 		].join(''));
 	} else {
