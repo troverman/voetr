468d9d76e11f6da61b3aa1d8d7cafc2d12a30e4e
Trevor Overman <troverman@gmail.com>
2017-03-25 22:11:57 -0400

    take a break

--- api/policies/enforceSsl.js
+++ api/policies/enforceSsl.js
@@ -1,12 +1,13 @@
-module.exports = function(req, response, next) {
+module.exports = function(req, res, next) {
 	'use strict';
-	if ((req.headers['x-forwarded-proto'] !== 'https') && (process.env.NODE_ENV === 'production')) {
-		return response.redirect([
-			'https://',
-			req.host,
-			req.url
-		].join(''));
-	} else {
+	//if ((req.headers['x-forwarded-proto'] !== 'https') && (process.env.NODE_ENV === 'production')) {
+		//return res.redirect([
+		//	'https://',
+		//	req.host,
+		//	req.url
+		//].join(''));
+		//res.redirect('http://google.com')
+	//} else {
 		next();
-	}
+	//}
 };
