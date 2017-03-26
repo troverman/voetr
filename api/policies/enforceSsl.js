6da8180ef0f31bf5bd8a1dc6f16566417b966446
Trevor Overman <troverman@gmail.com>
2017-03-25 23:59:30 -0400

    LLK

--- api/policies/enforceSsl.js
+++ api/policies/enforceSsl.js
@@ -1,7 +1,17 @@
 module.exports = function(req, res, next) {
 	//'use strict';
+
+	/*var app = express();
+	app.get('*',function(req,res,next){
+	  if(req.headers['x-forwarded-proto']!='https')
+	    res.redirect('https://mypreferreddomain.com'+req.url)
+	  else
+	    next() /* Continue to other routes if we're not redirecting */
+	//})*/
+
+
 	if ((req.headers['x-forwarded-proto'] !== 'https') && (process.env.NODE_ENV === 'production')) {
-		return res.redirect([
+		res.redirect([
 			'https://',
 			req.host,
 			req.url
