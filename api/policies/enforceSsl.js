660f9a5ffb45182a595c6146af58a1dcbde0b285
Trevor Overman <troverman@gmail.com>
2017-03-26 02:13:24 -0400

    uh

--- api/policies/enforceSsl.js
+++ api/policies/enforceSsl.js
@@ -1,6 +1,6 @@
 module.exports = function(req, res, next) {
 	//'use strict';
-	/*var express = require('express'),
+	var express = require('express'),
  	var app = express();
 	app.get('*',function(req,res,next){
 	if ((req.headers['x-forwarded-proto'] !== 'https') && (process.env.NODE_ENV === 'production')) {
@@ -12,20 +12,20 @@ module.exports = function(req, res, next) {
 
 	  else
 	    next() /* Continue to other routes if we're not redirecting */
-	//})*/
+	})
 
 
-	if ((req.headers['x-forwarded-proto'] !== 'https') && (process.env.NODE_ENV === 'production')) {
+	//if ((req.headers['x-forwarded-proto'] !== 'https') && (process.env.NODE_ENV === 'production')) {
 		/*res.redirect([
 			'https://',
 			req.host,
 			req.url
 		].join(''));*/
-		console.log(res)
-		res.status(301)
+		//console.log(res)
+		//res.status(301)
 		//res.status(301).redirect('https://www.google.com')
-	} 
-	else {
-		next();
-	}
+	//} 
+	//else {
+	//	next();
+	//}
 };
