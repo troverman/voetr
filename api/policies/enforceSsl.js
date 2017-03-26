87851bc6d9154a5f6c496fe10369288980cf6404
Trevor Overman <troverman@gmail.com>
2017-03-25 22:00:08 -0400

    noidea

--- api/policies/enforceSsl.js
+++ api/policies/enforceSsl.js
@@ -1,10 +1,9 @@
 module.exports = function(req, res, next) {
-	console.log(req)
 	'use strict';
 	if ((req.headers['x-forwarded-proto'] !== 'https') && (process.env.NODE_ENV === 'production')) {
 		return res.redirect([
 			'https://',
-			req.Host,
+			req.get('Host'),
 			req.url
 		].join(''));
 	} else {
