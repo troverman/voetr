4d8e6f5e64cad3289b346c48f46299d934f1b0e2
Trevor Overman <troverman@gmail.com>
2017-05-07 16:48:02 -0400

    hmm

--- api/models/Representative.js
+++ api/models/Representative.js
@@ -31,6 +31,25 @@ module.exports = {
         });
     },
 
+    afterCreate: function(model, next){
+        Representative.count()
+        .where({constituent: model.constituent})
+        .then(function(constituentCount){
+            User.update({id: model.constituent}, {representativeCount:constituentCount}).exec(function afterwards(err, updated){
+                User.publishUpdate(model.user, updated[0]);
+            });
+            Representative.count()
+            .where({representative: model.representative})
+            .then(function(representativeCount){
+                User.update({id: model.representative}, {constituentCount:representativeCount}).exec(function afterwards(err, updated){
+                    User.publishUpdate(model.user, updated[0]);
+                });
+                return next();
+            });
+        });
+    },
+
+
     getOne: function(id) {
         return Representative.findOne(id)
         .populate('representative')
--- api/models/VoteVote.js
+++ api/models/VoteVote.js
@@ -48,16 +48,27 @@ module.exports = {
         });
     },
 
-    /*afterCreate: function(model, next){
+    afterCreate: function(model, next){
         VoteVote.count()
         .where({user: model.user, bill:model.bill, vote:model.vote})
         .then(function(voteCount){
-            //get plus count, minus count -- 
-            Bill.update({id:model.bill, voteCount: voteCount}).then(function(){
-                return next(null, model);
+            Vote.find({id: model.vote}).then(function(voteModel){
+                if (req.param('voteInteger') == 1){voteModel[0].plusCount = voteModel[0].plusCount + 1;}
+                if (req.param('voteInteger') == -1){voteModel[0].minusCount = voteModel[0].minusCount + 1;}
+                voteModel[0].voteCount = voteVoteCount;
+                Vote.update({id: model.vote}, voteModel[0]).exec(function afterwards(err, updated){
+                    Vote.publishUpdate(model.vote, updated);
+                });
             });
+            Bill.update({id:model.bill, voteCount: voteCount}).exec(function afterwards(err, updated){
+                Bill.publishUpdate(model.bill, updated[0]);
+            });
+            User.update({id: model.user}, {voteCount:voteVoteCount}).exec(function afterwards(err, updated){
+                User.publishUpdate(req.param('user'), updated);
+            });
+            return next(null, model);
         });
-    },*/
+    },
 
     getOne: function(id) {
         return VoteVote.findOne(id)
