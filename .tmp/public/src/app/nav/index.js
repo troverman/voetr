eb33b151601378bfefdf18b0c968ec1716bef125
Trevor Overman <troverman@gmail.com>
2018-03-05 13:45:22 -0500

    refresh

--- .tmp/public/src/app/nav/index.js
+++
@@ -1,9 +0,0 @@
-angular.module( 'voetr.nav', [
-])
-
-.controller( 'NavCtrl',['$rootScope', '$scope', 'config', function NavController( $rootScope, $scope, config) {
-    $scope.currentUser = config.currentUser;
-   	$rootScope.$on("$stateChangeSuccess", function() {
-        window.scrollTo(0, 0);
-    });
-}]);
--- .tmp/public/src/common/services/config.js
+++
@@ -1,9 +0,0 @@
-angular.module( 'services.config', ['lodash'])
-.service('config',['lodash', function(lodash) {
-	return {
-		siteName: 'voetr',
-		siteUrl: '/',
-		apiUrl: '/api',
-		currentUser: false
-	};
-}]);
